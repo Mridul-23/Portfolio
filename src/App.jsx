997c5fb2c4d00fe96ccacf24d953641cac7772a9
Mridul <140313918+Mridul-23@users.noreply.github.com>
2025-12-06 10:56:19 +0530

    src: Update files

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { AnimatePresence, LayoutGroup } from 'framer-motion';
 import "./App.css";
 
 // Page Components
@@ -9,7 +8,6 @@ import Projects from './pages/Projects';
 import TechStack from './pages/TechStack';
 import Certifications from './pages/Certifications';
 import Social from './pages/Social';
-import Contact from './pages/Contact';
 import NotFound from './pages/NotFound';
 import Wrapper from './components/Wrapper';
 
@@ -17,24 +15,19 @@ import Wrapper from './components/Wrapper';
 export default function App() {
   return (  
     <Router>
-      <LayoutGroup>
-        <AnimatePresence exitBeforeEnter>
-          <Routes>
-            <Route path="/" element={<Wrapper />}>
-              <Route index element={<Home />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/projects" element={<Projects />} />
-              <Route path="/tech-stack" element={<TechStack />} />
-              <Route path="/certifications" element={<Certifications />} />
-              <Route path="/social" element={<Social />} />
-              <Route path="/contact" element={<Contact />} />
-            </Route>
-            {/* Redirect old routes or unknown */}
-            <Route path="/home" element={<Navigate to="/" replace />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </AnimatePresence>
-      </LayoutGroup>
+      <Routes>
+        <Route path="/" element={<Wrapper />}>
+          <Route index element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/tech-stack" element={<TechStack />} />
+          <Route path="/certifications" element={<Certifications />} />
+          <Route path="/social" element={<Social />} />
+        </Route>
+        {/* Redirect old routes or unknown */}
+        <Route path="/home" element={<Navigate to="/" replace />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </Router>
   );
 }
